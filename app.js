08d4f2b712a96ebfa745f1a97167b17c887bd67f
AndrewRusanov <rusanow.andrej2018@yandex.ru>
2024-02-08 17:11:29 +0300

    feat: create error center

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express, { urlencoded } from 'express';
 import mongoose from 'mongoose';
+import handleCenterError from './middlewares/centerError';
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -8,6 +9,8 @@ app.use(express.json());
 app.use(urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb');
+
+app.use(handleCenterError);
 
 app.listen(PORT, () => {
   console.log(`Server listen port ${PORT}`);
