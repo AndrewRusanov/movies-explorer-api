a3437364bf0ff31001fbcd95c1b7061fbf73c0f0
AndrewRusanov <rusanow.andrej2018@yandex.ru>
2024-02-13 19:47:20 +0300

    fix: refactor app

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 import express, { urlencoded } from 'express';
 import mongoose from 'mongoose';
-import cors from 'cors';
+
 import { errors } from 'celebrate';
 import dotenv from 'dotenv';
 import handleCenterError from './middlewares/centerError.js';
 import NotFoundError from './errors/NotFoundError.js';
 import router from './routes/index.js';
 import { requestLogger, errorLogger } from './middlewares/logger.js';
+import helmet from 'helmet';
 
 dotenv.config();
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 const app = express();
-app.use(cors());
+app.use(helmet());
 app.use(requestLogger);
 
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb');
+mongoose.connect(DB_URL);
 
 app.use(router);
 app.use('*', (req, res, next) => next(new NotFoundError('Страница не найдена')));
