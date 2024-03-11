5891f4c7570e78c97b3f65af57fe1e7ae8b0035c
AndrewRusanov <rusanow.andrej2018@yandex.ru>
2024-03-11 07:49:39 +0300

    fix cors problem

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-import express, { urlencoded } from 'express';
-import mongoose from 'mongoose';
+import express, { urlencoded } from "express";
+import mongoose from "mongoose";
 
-import { errors } from 'celebrate';
-import dotenv from 'dotenv';
-import handleCenterError from './middlewares/centerError.js';
-import NotFoundError from './errors/NotFoundError.js';
-import router from './routes/index.js';
-import { requestLogger, errorLogger } from './middlewares/logger.js';
-import helmet from 'helmet';
+import { errors } from "celebrate";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import handleCenterError from "./middlewares/centerError.js";
+import NotFoundError from "./errors/NotFoundError.js";
+import router from "./routes/index.js";
+import { requestLogger, errorLogger } from "./middlewares/logger.js";
 
 dotenv.config();
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 3000, DB_URL = "mongodb://127.0.0.1:27017/bitfilmsdb" } =
+  process.env;
 const app = express();
+app.use(cors());
 app.use(helmet());
 app.use(requestLogger);
 
@@ -21,7 +24,9 @@ app.use(urlencoded({ extended: true }));
 mongoose.connect(DB_URL);
 
 app.use(router);
-app.use('*', (req, res, next) => next(new NotFoundError('Страница не найдена')));
+app.use("*", (req, res, next) =>
+  next(new NotFoundError("Страница не найдена"))
+);
 app.use(errorLogger);
 app.use(errors());
 
